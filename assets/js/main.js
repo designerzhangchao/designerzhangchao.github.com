857c5f848e5be77ce39e17ed95d53e5cc9788e0d
designerzhangchao <designerzhangchao@126.com>
2016-08-29 14:14:53 +0800

    change

--- assets/js/main.js
+++ assets/js/main.js
@@ -120,7 +120,7 @@
 						overlayOpacity: 0.85,
 						popupCloserText: '',
 						popupLoaderText: '',
-						selector: '.work-item a.image',
+						selector: '.work-item a.gallery',
 						usePopupCaption: true,
 						usePopupDefaultStyling: false,
 						usePopupEasyClose: false,
